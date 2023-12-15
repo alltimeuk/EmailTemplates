938362f985ab7eca5f335b73d3842621fccb8826
Simon Jackson <simon.jackson@alltimetechnologies.co.uk>
2023-12-15 11:17:57 +0000

    check in a few templates

--- Intune/Detect-EmailSignatures.ps1
+++ Intune/Detect-EmailSignatures.ps1
@@ -9,36 +9,37 @@
     This PowerShell script is deployed as a detection script using Microsoft Intune remediations.
 
 .LINK
-    https://github.com/alltimeuk/EmailTemplates/blob/main/Intune/Detect-EmailSignatures.ps1
+    https://github.com/sjackson0109/Set-EmailSignatures/blob/main/Intune/Detect-EmailSignatures.ps1
 
+.LINK
+    https://github.com/Set-OutlookSignatures/Set-OutlookSignatures
+    
 .LINK
     https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/overview-endpoint-detection-response
 
 .NOTES
-    Version:        1.0.2
+    Version:        1.0.3
     Creation Date:  2023-11-07
-    Last Updated:   2023-11-08
+    Last Updated:   2023-12-11
     Author:         Simon Jackson / sjackson0109
-    Organization:   Alltime Technologies Ltd
-    Contact:        support@alltimetech.co.uk
-
+    Contact:        simon@jacksonfamily.me
 #>
 #Look in the localappdata\temp folder
 $temp = "$($env:localappdata)\temp"
-$file = "$temp\Set-OutlookSignatures.log"
+$logFile = "$temp\Set-OutlookSignatures.log"
+$addHours = 2 # Start with 2 hours, after a couple of weeks move it to 24 hours
 
 # Check if the log file exists
-If (Test-Path $file ){
-    Write-Host "Email Signatures LOG found."
-    If ( $(Get-Item $file).LastWriteTime -gt $(Get-Date).AddHours(-2) ) {
-        Write-Host "Log file written to within the last 2 hours. Exiting."
+If (Test-Path $logFile ){
+    If ( $(Get-Item $logFile).LastWriteTime -gt $(Get-Date).AddHours(-$addHours) ) {
+        Write-Host "NEW log found. Remediation NOT required"
         exit 0
     } Else {
-        Write-Host "Log is old. Remediation Required."
+        Write-Host "OLD log found. Remediation required."
         exit 1
     }
 }
 Else {
-    Write-Host "Email Signatures LOG not found. Remediation Required."
+    Write-Host "NO LOG found. Remediation required."
     exit 1
 }
--- Intune/Remediate-EmailSignatures.ps1
+++ Intune/Remediate-EmailSignatures.ps1
@@ -9,28 +9,33 @@
     This PowerShell script is deployed as a remediation script using Microsoft Intune remediations.
 
 .LINK
-    https://github.com/alltimeuk/EmailTemplates/blob/main/Intune/Remediate-EmailSignatures.ps1
+    https://github.com/sjackson0109/Set-EmailSignatures/blob/main/Intune/Remediate-EmailSignatures.ps1
+
+.LINK
+    https://github.com/Set-OutlookSignatures/Set-OutlookSignatures
 
 .LINK
     https://learn.microsoft.com/en-us/mem/intune/fundamentals/remediations
 
 .NOTES
-    Version:        1.0.2
+    Version:        1.0.3
     Creation Date:  2023-11-07
-    Last Updated:   2023-11-08
+    Last Updated:   2023-12-11
     Author:         Simon Jackson / sjackson0109
-    Organization:   Alltime Technologies Ltd
-    Contact:        support@alltimetech.co.uk
-
+    Contact:        simon@jacksonfamily.me
 #>
-$temp = $(Get-Location).path
-Start-Transcript $temp\Set-OutlookSignatures.log -Append
+$temp = "$($env:localappdata)\temp"
+$logFile = "$temp\Set-OutlookSignatures.log"
+Start-Transcript $logFile -Append
 
 # Variables for Download and Extract
 $githubProductOrg = "Set-OutlookSignatures"
 $githubProductRepo = "Set-OutlookSignatures"
+# $githubTemplateOrg = "sjackson0109"
+# $githubTemplateRepo = "Set-OutlookSignatures"
 $githubTemplateOrg = "alltimeuk"
 $githubTemplateRepo = "EmailTemplates"
+
 
 # Product Variables (standard)
 $graphOnly = "true"
@@ -38,74 +43,88 @@ $SetOofMsg = "false"
 $CreateRtfSignatures = "true"
 $CreateTxtSignatures = "true"
 $SignaturesForAutomappedAndAdditionalMailboxes = "true"
-$DeleteUserCreatedSignatures = "false"  #REQ TRUE FOR GO-LIVE
 
 # Product Variables (premium, req benefactor circle)
 $DocxHighResImageConversion = "false"
 $SetCurrentUserOutlookWebSignature = "true"
-$MirrorLocalSignaturesToCloud = "true" #not used
-$DeleteScriptCreatedSignaturesWithoutTemplate = "false" #not used
+$MirrorLocalSignaturesToCloud = "true"
+$DeleteUserCreatedSignatures = "true"  #REQ TRUE FOR GO-LIVE
+$DeleteScriptCreatedSignaturesWithoutTemplate = "true"
 
 
 # Init
 # Obtain the latest release off each github project  -- note: latest is always array item 0
-$productUrl = ((Invoke-WebRequest -Uri "https://api.github.com/repos/$githubProductOrg/$githubProductRepo/releases/latest" -UseBasicParsing | ConvertFrom-Json).assets | Where-Object { $_.name -ilike 'Set-OutlookSignatures_v*.zip' })[0]
-$templateUrl = ((Invoke-WebRequest -Uri "https://api.github.com/repos/$githubTemplateOrg/$githubTemplateRepo/releases/latest" -UseBasicParsing | ConvertFrom-Json).assets | Where-Object { $_.name -ilike 'Set-OutlookSignatures_v*.zip' })
-$productMeta = Invoke-WebRequest $productUrl.browser_download_url
-$templateMeta = Invoke-WebRequest $templateUrl.browser_download_url
+$productRelease = Invoke-WebRequest -Uri "https://api.github.com/repos/$githubProductOrg/$githubProductRepo/releases/latest" -UseBasicParsing | ConvertFrom-Json
+$productUrl = $productRelease.assets.browser_download_url
+$productPublished = $productRelease.published_at
+$productVersion = $productRelease.tag_name
+
+$templateRelease = Invoke-WebRequest -Uri "https://api.github.com/repos/$githubTemplateOrg/$githubTemplateRepo/releases/latest" -UseBasicParsing | ConvertFrom-Json
+$templateUrl = $templateRelease.zipball_url
+$templatePublished = $templateRelease.published_at
+$templateVersion = $templateRelease.tag_name
 
 # Specify the file-system of the downloaded targets
-$productZip = "$temp\Set-OutlookSignatures.zip"
-$templateZip = "$temp\EmailTemplates.zip"
-$productPath = "$temp\$githubProductOrg-$githubProductRepo-$($($productMeta.commit.sha).substring(0,7))"
-$templatePath = "$temp\$githubTemplateOrg-$githubTemplateRepo-$($($templateMeta.commit.sha).substring(0,7))" 
+$productRelease | Out-File "$githubProductRepo.json"
+$productZip = "$githubProductRepo-$productVersion.zip"
+$productPath = "$githubProductRepo-$productVersion"
+
+$templateRelease | Out-File "$githubTemplateRepo.json"
+$templateZip = "$githubTemplateRepo-$templateVersion.zip"
+$templatePath = "$githubTemplateOrg-$githubTemplateRepo-$templateVersion" 
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 
 # Check if the latest version is already downloaded, clean up the file-system and download+extract, or just extract again
-If (Test-Path $productPath){
+If (Test-Path "$temp\$productPath"){
     Write-Host "Cleaning up local path $productPath"
     Remove-Item $productPath -recurse -Force
 } else {
+    Write-Host "Creating product folder"
+    New-Item $productPath -ErrorAction SilentlyContinue
     Write-Host "Downloading $productUrl to $productZip"
-    Invoke-WebRequest "$($productMeta.zipball_url)" -Out $productZip
+    Invoke-WebRequest $productUrl -Out $productZip
 }
 
-If (Test-Path $templatePath){
+If (Test-Path "$temp\$templatePath"){
     Write-Host "Cleaning up local path $templatePath"
     Remove-Item $templatePath -recurse -Force
 } else {
     Write-Host "Downloading $templateUrl to $templateZip"
-    Invoke-WebRequest "$($templateMeta.zipball_url)" -Out $templateZip
+    Invoke-WebRequest $templateUrl -Out $templateZip
 }
-Write-host "==============="
-Get-ChildItem $temp
+Write-host "===============" 
+Get-ChildItem .\
 Write-host "==============="
 
 
 # A fresh Extraction of the zipball files to the temp directory, filename encoding needs converting to ascii, not utf8.
 # Note: some errors with file-name length when testing with my user docs area. C:\WINDOWS\IMECache\HealthScripts\(GUID)\ is just as long, so skip errors. Only signature samples anyway, don't need them.
-Write-Host "Extracting $productZip to $temp"
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$productZip", "$temp", [System.Text.Encoding]::ascii) | out-null
-Write-Host "Extracting $templateZip to $temp"
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$templateZip", "$temp", [System.Text.Encoding]::ascii) | out-null
+Write-Host "Extracting $productZip"
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$temp\$productZip", "$temp\", [System.Text.Encoding]::ascii)| out-null
+Write-Host "Extracting $templateZip"
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$temp\$templateZip", "$temp\", [System.Text.Encoding]::ascii) | out-null
+#Expand-Archive -path "$temp\$templateZip" -destinationPath "$temp\$targetPath" | out-null
 
 Write-host "==============="
 Get-ChildItem $temp
 Write-host "==============="
 
 # Gather some path data
-$templateTargetPath = "$temp\$githubTemplateOrg-$githubTemplateRepo-$($($templateMeta.commit.sha).substring(0,7))"
-$executionPath = "$githubProductOrg-$githubProductRepo-$($($productMeta.commit.sha).substring(0,7))\src_Set-OutlookSignatures"
+$productLocation = "$( $productPath -replace '-v' , '_v' )"
+$templateFolderPrefix = "$githubTemplateOrg-$gitHubTemplateRepo"
+$templateExtracted = $(Get-ChildItem $temp -Directory -Recurse -Depth 1| ? { $_.Name -match "^$templateFolderPrefix" } | Sort LastWriteTime)[0].Name
+$templateLocation = "$temp\$templateExtracted"
+
 # Clean up the downloaded content
 #Remove-Item -Path $productZip -Force
 #Remove-Item -Path $templateZip -Force
 
 
 #Run product, with transcript logging, and args passed from variables above
-Set-Location $temp\$executionPath
-.\Set-OutlookSignatures.ps1 -graphonly $graphOnly -SignatureTemplatePath $templateTargetPath\Signatures -SignatureIniPath $templateTargetPath\Signatures\_Signatures.ini -SetCurrentUserOOFMessage $SetOofMsg -CreateRtfSignatures $CreateRtfSignatures -CreateTxtSignatures $CreateTxtSignatures -SignaturesForAutomappedAndAdditionalMailboxes $SignaturesForAutomappedAndAdditionalMailboxes -DisableRoamingSignatures $DisableRoamingSignatures -SetCurrentUserOutlookWebSignature $SetCurrentUserOutlookWebSignature -DeleteUserCreatedSignatures $DeleteUserCreatedSignatures -DeleteScriptCreatedSignaturesWithoutTemplate $DeleteScriptCreatedSignaturesWithoutTemplate
-Set-Location $temp
-Stop-Transcript 
+Set-Location "$temp\$productLocation"
+.\Set-OutlookSignatures.ps1 -graphonly $graphOnly -SignatureTemplatePath $templateLocation\Signatures -SignatureIniPath $templateLocation\Signatures\_Signatures.ini -SetCurrentUserOOFMessage $SetOofMsg -CreateRtfSignatures $CreateRtfSignatures -CreateTxtSignatures $CreateTxtSignatures -SignaturesForAutomappedAndAdditionalMailboxes $SignaturesForAutomappedAndAdditionalMailboxes -DisableRoamingSignatures $DisableRoamingSignatures -SetCurrentUserOutlookWebSignature $SetCurrentUserOutlookWebSignature -DeleteUserCreatedSignatures $DeleteUserCreatedSignatures -DeleteScriptCreatedSignaturesWithoutTemplate $DeleteScriptCreatedSignaturesWithoutTemplate
+Set-Location "$temp"
+Stop-Transcript
 exit 0
